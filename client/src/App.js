37e812ac62124263743e43f1248499a1fe2e2b57
Enrique Lopez <elop22@gmail.com>
2020-11-22 22:17:31 -0700

    debuggin

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment }from 'react';
+import React from 'react';
 import BookContainer from './components/BookContainer'
 import './App.css';
 
--- client/src/utils/BookContext.js
+++ client/src/utils/BookContext.js
@@ -3,12 +3,8 @@ import React from 'react';
 const BookContext = React.createContext({
   result: {},
   search: '',
-  handleInputChange: (e) =>{
-    e.preventDefault();
-  },
-  handleFormSubmit: (e) => {
-    e.preventDefault();
-  }
+  handleInputChange: () =>{},
+  handleFormSubmit: () => {}
 });
 
 export default BookContext;
